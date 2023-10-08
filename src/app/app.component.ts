ec44f9f30b9d30b2a7d46b4c1fcc75d9745eaafe
fusho-takahashi <zenryaku@fusho-takahashi.me>
2023-10-08 14:33:25 +0900

    API から取得した値を画面表示に反映する

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { hc } from 'hono/client';
+import { ApiRoute } from '../../functions';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-cfp-rpc';
+  client = hc<ApiRoute>('http://127.0.0.1:8788/api');
+
+  async ngOnInit() {
+    const res = await this.client.title.$get();
+    const data = await res.json();
+    this.title = data.title;
+  }
 }
